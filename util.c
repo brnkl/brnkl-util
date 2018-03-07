6c4439816a29e15f3408922d7ca144fd4bf71671
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-03-06 21:27:28 -0800

    Use chromium clang format

--- util.c
+++ util.c
@@ -1,40 +1,45 @@
-#include "legato.h"
 #include "util.h"
+#include "legato.h"
 
-le_result_t readFromFile (const char *path, void *value,
-  int (*scanCallback) (FILE *f, void *val)) {
-  FILE *f = fopen(path, "r");
+le_result_t readFromFile(const char* path,
+                         void* value,
+                         int (*scanCallback)(FILE* f, void* val)) {
+  FILE* f = fopen(path, "r");
   if (f == NULL) {
     LE_WARN("Couldn't open '%s' - %m", path);
     return LE_IO_ERROR;
   }
   int numScanned = scanCallback(f, value);
-  if (numScanned != 1) return LE_FORMAT_ERROR;
+  if (numScanned != 1)
+    return LE_FORMAT_ERROR;
   fclose(f);
   return LE_OK;
 }
 
-int scanIntCallback (FILE *f, void *value) {
-  int *val = value;
+int scanIntCallback(FILE* f, void* value) {
+  int* val = value;
   return fscanf(f, "%d", val);
 }
 
-int scanDoubleCallback (FILE *f, void *value) {
-  double *val = value;
+int scanDoubleCallback(FILE* f, void* value) {
+  double* val = value;
   return fscanf(f, "%lf", val);
 }
 
-le_result_t ioutil_readIntFromFile (const char *path, int *value) {
+le_result_t ioutil_readIntFromFile(const char* path, int* value) {
   return readFromFile(path, value, scanIntCallback);
 }
 
-le_result_t ioutil_readDoubleFromFile (const char *path, double *value) {
+le_result_t ioutil_readDoubleFromFile(const char* path, double* value) {
   return readFromFile(path, value, scanDoubleCallback);
 }
 
-le_result_t ioutil_writeToFile (const char *path, void *value, size_t size, size_t count) {
+le_result_t ioutil_writeToFile(const char* path,
+                               void* value,
+                               size_t size,
+                               size_t count) {
   LE_INFO("Attempting to write to %s", path);
-  FILE *f = fopen(path, "w");
+  FILE* f = fopen(path, "w");
   if (f == NULL) {
     LE_WARN("Failed to open %s for writing", path);
     return LE_IO_ERROR;
@@ -45,15 +50,16 @@ le_result_t ioutil_writeToFile (const char *path, void *value, size_t size, size
   return LE_OK;
 }
 
-le_result_t util_flattenRes (le_result_t *res, int nRes) {
+le_result_t util_flattenRes(le_result_t* res, int nRes) {
   for (int i = 0; i < nRes; i++) {
-    le_result_t *resPtr = res + i;
-    if (*resPtr != LE_OK) return *resPtr;
+    le_result_t* resPtr = res + i;
+    if (*resPtr != LE_OK)
+      return *resPtr;
   }
   return LE_OK;
 }
 
-int util_getUnixDatetime () {
+int util_getUnixDatetime() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return tv.tv_sec;
@@ -63,15 +69,18 @@ int util_getUnixDatetime () {
  * Get the last modified datetime
  * of the file at path
  */
-time_t util_getMTime (char *path) {
+time_t util_getMTime(char* path) {
   struct stat st;
-  if (stat(path, &st) != 0) return -1;
-  else return st.st_mtime;
+  if (stat(path, &st) != 0)
+    return -1;
+  else
+    return st.st_mtime;
 }
 
 uint64_t GetCurrentTimestamp(void) {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  uint64_t utcMilliSec = (uint64_t)(tv.tv_sec) * 1000 + (uint64_t)(tv.tv_usec) / 1000;
+  uint64_t utcMilliSec =
+      (uint64_t)(tv.tv_sec) * 1000 + (uint64_t)(tv.tv_usec) / 1000;
   return utcMilliSec;
 }
--- util.h
+++ util.h
@@ -7,17 +7,21 @@
 #define LOW 0
 
 // ioutil
-le_result_t readFromFile (const char *path, void *value,
-  int (*scanCallback) (FILE *f, void *val));
-int scanIntCallback (FILE *f, void *value);
-int scanDoubleCallback (FILE *f, void *value);
-le_result_t ioutil_readIntFromFile (const char *path, int *value);
-le_result_t ioutil_readDoubleFromFile (const char *filePath, double *value);
-le_result_t ioutil_writeToFile (const char *path, void *value, size_t size, size_t count);
+le_result_t readFromFile(const char* path,
+                         void* value,
+                         int (*scanCallback)(FILE* f, void* val));
+int scanIntCallback(FILE* f, void* value);
+int scanDoubleCallback(FILE* f, void* value);
+le_result_t ioutil_readIntFromFile(const char* path, int* value);
+le_result_t ioutil_readDoubleFromFile(const char* filePath, double* value);
+le_result_t ioutil_writeToFile(const char* path,
+                               void* value,
+                               size_t size,
+                               size_t count);
 
-uint64_t GetCurrentTimestamp (void);
-time_t util_getMTime (char *path);
-int util_getUnixDatetime ();
-le_result_t util_flattenRes (le_result_t *res, int nRes);
+uint64_t GetCurrentTimestamp(void);
+time_t util_getMTime(char* path);
+int util_getUnixDatetime();
+le_result_t util_flattenRes(le_result_t* res, int nRes);
 
 #endif
